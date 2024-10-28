b15a00d3687d22176128e7ac05f1ba42784992bb
priyam242 <ppriyamprajapati@gmail.com>
2024-10-28 13:16:06 +0530

    Delete modul 3 directory

--- modul 3/modul 3.1/question 1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    printf("\n<---my bio-data--->");	
-	printf("\nmy name is priyam prajapati");
-	printf("\nmy birthdate = 05/05/2007");
-	printf("\nmy age = 17 years");
-	printf("\nmy address =119 nirmal park society ,s.p ring road odhav, ahemdabad");
-}
--- modul 3/modul 3.2/question 3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int num;
-	printf("\nenter the value of num:");
-	scanf("%d",&num);
-	
-	(num%2==0 ) ? printf("\n%d is the even number",num) : printf("\n%d is the odd number",num) ;
-}
